86bfc0e28f5efcea067914ebe5fd67375b026b7a
Leo Babakhanian <babakhanianleo@gmail.com>
2022-08-18 01:12:13 -0700

    symmetricTree solution

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -1,4 +1,32 @@
 
 public class SymmetricTree {
-
+	
+	// Definition for a binary tree node.
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	public boolean isSymmetric(TreeNode root) {
+        
+        return isSymmetric(root, root);
+    }
+    
+    public boolean isSymmetric(TreeNode t1, TreeNode t2){
+        
+        if(t1 == null && t2 == null)
+            return true;
+        if(t1 == null || t2 == null)
+            return false;
+        
+        return (t1.val == t2.val) && isSymmetric(t1.left, t2.right) && isSymmetric(t1.right, t2.left);
+    }
 }
