3402988d160d434a4cc6bee6283e93dd1aa42d72
Leo Babakhanian <babakhanianleo@gmail.com>
2022-10-27 13:15:16 -0700

    updateMatrix solution

--- src/UpdateMatrix.java
+++ src/UpdateMatrix.java
@@ -6,7 +6,7 @@ public class UpdateMatrix {
 		int count = 0;
 		
 		for(int i = 0; i < mat.length; i++) {
-			for(int j = 0; j < mat[0].length; i++) {
+			for(int j = 0; j < mat[0].length; j++) {
 				if(mat[i][j] == 1) {
 					mat[i][j] = -1;
 					count++;
